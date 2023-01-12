f56b6fd7e1b668a8649ac956409c53b049e7c472
cbk914 <cbk914@riseup.net>
2023-01-12 01:08:34 +0100

    Update daily-update.py

--- daily-update.py
+++ daily-update.py
@@ -1,42 +1,47 @@
+
+
+
 # Add ´0 0 * * * /usr/bin/python3 /path/to/file/daily-update.py´ to contrab
 import urllib.request
 import datetime
-import gzip
-import os
-from pymongo import MongoClient
-import csv
+import locale
 
 url = "https://epss.cyentia.com/epss_scores-current.csv.gz"
 now = datetime.datetime.now()
 file_name = "epss_scores-" + now.strftime("%Y-%m-%d") + ".csv.gz"
+urllib.request.urlretrieve(url, file_name)
 
-try:
-    urllib.request.urlretrieve(url, file_name)
-    print("Succesfully downloaded as " + file_name)
+print("Succesfully downloaded as " + file_name)
 except Exception as e:
     print("Error downloading file: " + str(e))
 
 if "CRON" not in os.environ:
-    answer = input("¿Uncompress the file? (y/n) ")
-    if answer.lower() == "y":
-        try:
-            with gzip.open(file_name, 'rb') as f_in:
-                with open(file_name.strip('.gz'), 'wb') as f_out:
+    current_locale, encoding = locale.getdefaultlocale()
+    if current_locale in ["es_ES", "es_MX", "es_PE", "es_VE"]:
+        answer = input("¿Desea descomprimir el archivo? (s/n): ")
+        if answer.lower() == "s":
+            with gzip.open(gz_file_name, 'rb') as f_in:
+                with open(file_name, 'wb') as f_out:
                     f_out.writelines(f_in)
-            print(f"File {file_name} uncompressed and saved as {file_name.strip('.gz')}")
-        except Exception as e:
-            print(f"Error uncompressing file: {str(e)}")
-        try:
-            client = MongoClient()
-            db = client['epss-scores']
-            collection = db.epss_scores
-
-            with open(file_name.strip('.gz'), 'r') as file:
-                reader = csv.DictReader(file)
-                for row in reader:
-                    collection.insert_one(row)
-            print("File Loaded Successfully to Mongo DB")
-        except Exception as e:
-            print(f"Error Loading file to Mongo DB: {str(e)}")
-else:
-    print("CRON job detected, no interaction available")
+            print(f"Archivo {gz_file_name} descomprimido y guardado como {file_name}")
+    elif current_locale in ["fr_FR"]:
+        answer = input("Voulez-vous décompresser le fichier? (o/n): ")
+        if answer.lower() == "o":
+            with gzip.open(gz_file_name, 'rb') as f_in:
+                with open(file_name, 'wb') as f_out:
+                    f_out.writelines(f_in)
+            print(f"Archivo {gz_file_name} décompressé et enregistré en tant que {file_name}")
+    elif current_locale in ["nl_NL"]:
+        answer = input("Wilt u het bestand uitpakken? (j/n): ")
+        if answer.lower() == "j":
+            with gzip.open(gz_file_name, 'rb') as f_in:
+                with open(file_name, 'wb') as f_out:
+                    f_out.writelines(f_in)
+            print(f"Bestand {gz_file_name} uitgepakt en opgeslagen als {file_name}")            
+    else:
+        answer = input("Do you want to uncompress the file? (y/n): ")
+        if answer.lower() == "y":
+            with gzip.open(gz_file_name, 'rb') as f_in:
+                with open(file_name, 'wb') as f_out:
+                    f_out.writelines(f_in)
+            print(f"File {gz_file_name} uncompressed and saved as {file_name}")
