3648beb289008f1a1dd1cab7ded477319ebad665
cbk914 <cbk914@riseup.net>
2023-02-19 21:20:41 +0100

    Update daily-update.py

--- daily-update.py
+++ daily-update.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Author: cbk914
-# Add ´0 0 * * * /usr/bin/python3 /path/to/file/daily-update.py´ to contrab
+# Add ´0 0 * * * /usr/bin/python3 /path/to/file/daily-update.py´ to crontrab
 import urllib.request
 import datetime
 import gzip
